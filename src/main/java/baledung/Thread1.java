f8fc226106da91caca28ae8185e4ad207de7bd3b
Shrikar <Shrikar@citiustech.com>
2021-10-27 10:37:10 +0530

    Async impl

---
+++ src/main/java/baledung/Thread1.java
@@ -0,0 +1,20 @@
+package baledung;
+
+public class Thread1 {
+    public static void main(String[] args) {
+
+        int number = 12;
+        Thread newThread = new Thread(() -> {
+            System.out.println("Factorial of " + number + " is: " + factorial(number));
+        });
+        newThread.start();
+    }
+    public static int factorial(int number){
+        int i,fact=1;
+
+        for(i=1;i<=number;i++){
+            fact=fact*i;
+        }
+        return fact;
+    }
+}
